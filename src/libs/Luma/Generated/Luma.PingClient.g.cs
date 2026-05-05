767351ef5397f714deca21503c6208bbe96bc872
Konstantin S. <havendv@gmail.com>
2026-05-05 20:26:43 +0400

    feat: Updated OpenAPI spec (#163)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Luma/Generated/Luma.PingClient.g.cs
+++ src/libs/Luma/Generated/Luma.PingClient.g.cs
@@ -72,10 +72,10 @@ namespace Luma
         /// <param name="options">Client-wide request defaults such as headers, query parameters, retries, and timeout.</param>
         /// <param name="disposeHttpClient">Dispose the HttpClient when the instance is disposed. True by default.</param>
         public PingClient(
-            global::System.Net.Http.HttpClient? httpClient = null,
-            global::System.Uri? baseUri = null,
-            global::System.Collections.Generic.List<global::Luma.EndPointAuthorization>? authorizations = null,
-            global::Luma.AutoSDKClientOptions? options = null,
+            global::System.Net.Http.HttpClient? httpClient,
+            global::System.Uri? baseUri,
+            global::System.Collections.Generic.List<global::Luma.EndPointAuthorization>? authorizations,
+            global::Luma.AutoSDKClientOptions? options,
             bool disposeHttpClient = true)
         {
 
