c8b2f47243cfbdfedbcaaa50830dff142f8c4e09
Konstantin S. <havendv@gmail.com>
2026-09-13 19:39:01 +0400

    feat: Updated OpenAPI spec (#186)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Luma/Generated/Luma.PingClient.g.cs
+++ src/libs/Luma/Generated/Luma.PingClient.g.cs
@@ -34,7 +34,7 @@ namespace Luma
         /// <inheritdoc/>
         public global::Luma.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Luma.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Luma.PingSourceGenerationContext.Default);
 
         /// <summary>
         ///
